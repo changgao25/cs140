50b51207c4dc06009d8200aa693f75442013dcbc
Kira Minkova <kminkova@gmail.com>
2024-06-20 23:47:23 -0700

    test

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,9 @@
-//
-// Created by aknight on 8/2/19.
-//
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
+
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
